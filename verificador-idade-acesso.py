283949cf94a4a6286dfde2bc9367ef4477a93f32
Janaina Maria <108072378+Janaina-maria@users.noreply.github.com>
2026-01-13 20:09:15 -0300

    Projeto Python

---
+++ verificador-idade-acesso.py
@@ -0,0 +1,8 @@
+idade = float('Informe sua idade: ')
+
+if idade <12:
+    print('Conteúdo Infantil')
+elif idade >12 and idade <=17:
+    print('Conteúdo Juvenil')
+else:
+    print('Conteúdo Adulto')
